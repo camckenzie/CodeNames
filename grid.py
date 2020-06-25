8b2f259a4af702d74dc95041612d26a0db92c32f
camckenzie <63214003+camckenzie@users.noreply.github.com>
2020-06-25 18:50:03 -0400

    Testing

--- grid.py
+++ grid.py
@@ -1,8 +1,8 @@
 from card import Card
 from random import randint
 from word import words
+from prettytable import PrettyTable
 
-print('something')
 class Grid:
     
 
@@ -20,9 +20,25 @@ class Grid:
             self.cards.append(new_card)
     
     def display_grid(self):
+        print()
         for card in self.cards:
             print(card.getText())
 
+    
+    # def info(self) -> List[str]:
+        
+    #     #return []
+    
+    # def grid_table(self) -> None:
+
+    #     pt: PrettyTable = PrettyTable(field_names='1', '2', '3', '4', '5')
+
+    #     for card in self.cards:
+    #         pt.add_column('test',card.getText())
+    #     print('GRID THING')
+
 test = Grid()
 test.display_grid()
+
+#test.grid_table()
 
